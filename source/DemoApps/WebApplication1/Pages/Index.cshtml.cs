4bd82b9b7f5e9981af1bcdf853b423ec4a077c11
Ayumu Inaba <ainaba@microsoft.com>
2018-05-22 15:18:10 +0900

    DBアクセス追加

--- source/DemoApps/WebApplication1/Pages/Index.cshtml.cs
+++ source/DemoApps/WebApplication1/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +10,46 @@ namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
+        public class Employee
+        {
+            public int EmployeeId { get; set; }
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+        }
+
+        public List<Employee> Employees = new List<Employee>();
+        
         public void OnGet()
         {
+            var constr = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=demo0517;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (var con = new SqlConnection(constr))
+            {
+                using (var cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = "select * from employee";
 
+                    con.Open();
+                    try
+                    {
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while(reader.Read())
+                            {
+                                this.Employees.Add(new Employee() {
+                                    EmployeeId = reader.GetInt32( reader.GetOrdinal("employeeid")),
+                                    Firstname = reader.GetString( reader.GetOrdinal("firstname")),
+                                    Lastname = reader.GetString( reader.GetOrdinal("lastname"))
+                                    });
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        con.Close();
+                    }
+
+                }
+            }
         }
     }
 }
