00d4f553b4dd14b5af7d48c96a5164507dcec7bb
Ayumu Inaba <ainaba@microsoft.com>
2018-05-23 19:54:57 +0900

    スクリプトの調整

---
+++ deploy/windows/vm-webserver/setup-middle.ps1
@@ -0,0 +1,10 @@
+#install iis
+Add-WindowsFeature Web-Server
+
+#install asp.net core hosting bundle
+#https://www.microsoft.com/net/download/dotnet-core/runtime-2.0.7
+Invoke-WebRequest -UseBasicParsing -Uri "https://download.microsoft.com/download/A/9/F/A9F8872C-48B2-41DB-8AAD-D5908D988592/DotNetCore.2.0.7-WindowsHosting.exe" -OutFile "C:\DotNetCore.2.0.7-WindowsHosting.exe"
+Start-Process "C:\DotNetCore.2.0.7-WindowsHosting.exe" -ArgumentList @("/quiet", "/log", "log.txt") -NoNewWindow
+
+#restart iis
+Start-Process "iisreset" -ArgumentList @() -NoNewWindow
