15f852729819a8f14fe881d96cbe402c2cbbc79f
Ayumu Inaba <ainaba@microsoft.com>
2018-05-23 18:44:47 +0900

    ビルドステージの分割

--- deploy/windows/app/publish.cmd
+++ deploy/windows/app/publish.cmd
@@ -3,12 +3,6 @@
 setlocal
 cd %~dp0
 
-set source="%~dp0\..\..\..\source\DemoApps"
-set output="%~dp0\publish"
-
-rmdir /S /Q %output%
-dotnet publish %source% -o %output% -f netcoreapp2.0 -r win-x64 --self-contained
-
 docker build -t aspnetcore-demoapp .
 
 endlocal
--- deploy/windows/baseimage/publish.cmd
+++ deploy/windows/baseimage/publish.cmd
@@ -8,4 +8,6 @@ endlocal
 
 
 REM ================================================
-REM 
+REM docker tag aspnetcore-iishosting ayuina/aspnetcore-iishosting
+REM docker login
+REM docker push ayuina/aspnetcore-iishosting
