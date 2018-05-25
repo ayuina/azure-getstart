f17a8eddcef7a58d7d052b0da5799ee6a6bc911a
Ayumu Inaba <ainaba@microsoft.com>
2018-05-25 18:07:34 +0900

    ZIP 圧縮の追加

--- deploy/windows/local/publish.cmd
+++ deploy/windows/local/publish.cmd
@@ -7,7 +7,8 @@ set source="%~dp0\..\..\..\source\DemoApps"
 set output="%~dp0\publish"
 dotnet publish %source% -f netcoreapp2.0 -r win-x64 --self-contained -o %output%
 
-
+echo compressing
+powershell.exe Compress-Archive .\publish\* -DestinationPath .\publish.zip
 
 endlocal
 
